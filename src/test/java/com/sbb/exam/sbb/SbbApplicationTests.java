6208be93b9b22a9ff8dfc646dfbc9ff5c241256a
yup2.dev <126732440+yup2dev@users.noreply.github.com>
2023-08-02 01:45:33 +0900

    test delete

--- src/test/java/com/sbb/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbb/exam/sbb/SbbApplicationTests.java
@@ -12,16 +12,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
-
-	@Autowired
-	private QuestionService questionService;
-
-	@Test
-	void testJpa() {
-		for (int i = 1; i <= 300; i++) {
-			String subject = String.format("테스트 데이터입니다:[%03d]", i);
-			String content = "내용무";
-			this.questionService.create(subject, content, null);
-		}
-	}
 }
